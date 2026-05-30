5f1647c395eabba140346994c192323c1d0a86a8
Kelompok4Vinix7 <kelompok4vinix7@gmail.com>
2026-05-30 18:14:21 +0000

    update commit simulasi gitleaks

--- app.py
+++ app.py
@@ -4,6 +4,9 @@
 DB_PASSWORD = "super-secret-password-123"
 API_KEY = "AIzaSyA-ContohApiKeyGCP-12345"
 
+# Key panjang untuk simulai gitleaks
+DATABASE_URL = "postgres://admin:p4ssw0rd_rahasia_banget@db.example.com:5432/mydb"
+
 def login():
     print(f"Menghubungkan ke database dengan password: {DB_PASSWORD}")
 
